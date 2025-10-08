caa9a0e3c09ca1950528fc47fa23fff47f8c53cf
Shaik Farid <shaikfarid524@gmail.com>
2025-10-08 19:18:21 +0530

    Update BankManagementSystemApplication.java

--- src/main/java/com/alien/bank/management/system/BankManagementSystemApplication.java
+++ src/main/java/com/alien/bank/management/system/BankManagementSystemApplication.java
@@ -2,12 +2,18 @@ package com.alien.bank.management.system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BankManagementSystemApplication {
+public class BankManagementSystemApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BankManagementSystemApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BankManagementSystemApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(BankManagementSystemApplication.class, args);
+    }
 }
